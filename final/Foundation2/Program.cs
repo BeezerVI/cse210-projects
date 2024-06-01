9c22aab9ac0b4488969bb40b7d774ea697c24ecb
BeezerVI <hyrummckay@gmail.com>
2024-06-01 15:08:16 -0600

    minor fixses

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
         Order order1 = new Order(1);
         Order order2 = new Order(2);
         Order order3 = new Order(3);
